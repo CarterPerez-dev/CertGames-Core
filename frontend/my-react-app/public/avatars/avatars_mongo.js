218e71f04b1b82f8fd3d7550c76000d02029b7ee
Carter Perez <cartertechsolution@gmail.com>
2025-02-06 16:12:17 -0500

    Create avatars_mongo.js

---
+++ frontend/my-react-app/public/avatars/avatars_mongo.js
@@ -0,0 +1,299 @@
+db.shopItems.insertMany([
+  {
+    type: "avatar",
+    title: "avatar1",
+    description: "Avatar 1",
+    cost: 10,
+    imageUrl: "/avatars/avatar1.png",
+    unlockLevel: 5,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar2",
+    description: "Avatar 2",
+    cost: 10,
+    imageUrl: "/avatars/avatar2.png",
+    unlockLevel: 8,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar3",
+    description: "Avatar 3",
+    cost: 10,
+    imageUrl: "/avatars/avatar3.png",
+    unlockLevel: 12,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar4",
+    description: "Avatar 4",
+    cost: 10,
+    imageUrl: "/avatars/avatar4.png",
+    unlockLevel: 16,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar5",
+    description: "Avatar 5",
+    cost: 10,
+    imageUrl: "/avatars/avatar5.png",
+    unlockLevel: 20,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar6",
+    description: "Avatar 6",
+    cost: 10,
+    imageUrl: "/avatars/avatar6.png",
+    unlockLevel: 24,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar7",
+    description: "Avatar 7",
+    cost: 10,
+    imageUrl: "/avatars/avatar7.png",
+    unlockLevel: 28,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar8",
+    description: "Avatar 8",
+    cost: 10,
+    imageUrl: "/avatars/avatar8.png",
+    unlockLevel: 32,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar9",
+    description: "Avatar 9",
+    cost: 10,
+    imageUrl: "/avatars/avatar9.png",
+    unlockLevel: 36,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar10",
+    description: "Avatar 10",
+    cost: 10,
+    imageUrl: "/avatars/avatar10.png",
+    unlockLevel: 40,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar11",
+    description: "Avatar 11",
+    cost: 10,
+    imageUrl: "/avatars/avatar11.png",
+    unlockLevel: 44,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar12",
+    description: "Avatar 12",
+    cost: 10,
+    imageUrl: "/avatars/avatar12.png",
+    unlockLevel: 48,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar13",
+    description: "Avatar 13",
+    cost: 10,
+    imageUrl: "/avatars/avatar13.png",
+    unlockLevel: 52,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar14",
+    description: "Avatar 14",
+    cost: 10,
+    imageUrl: "/avatars/avatar14.png",
+    unlockLevel: 56,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar15",
+    description: "Avatar 15",
+    cost: 10,
+    imageUrl: "/avatars/avatar15.png",
+    unlockLevel: 60,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar16",
+    description: "Avatar 16",
+    cost: 10,
+    imageUrl: "/avatars/avatar16.png",
+    unlockLevel: 64,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar17",
+    description: "Avatar 17",
+    cost: 10,
+    imageUrl: "/avatars/avatar17.png",
+    unlockLevel: 68,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar18",
+    description: "Avatar 18",
+    cost: 10,
+    imageUrl: "/avatars/avatar18.png",
+    unlockLevel: 72,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar19",
+    description: "Avatar 19",
+    cost: 10,
+    imageUrl: "/avatars/avatar19.png",
+    unlockLevel: 76,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar20",
+    description: "Avatar 20",
+    cost: 10,
+    imageUrl: "/avatars/avatar20.png",
+    unlockLevel: 80,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar21",
+    description: "Avatar 21",
+    cost: 10,
+    imageUrl: "/avatars/avatar21.png",
+    unlockLevel: 84,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar22",
+    description: "Avatar 22",
+    cost: 10,
+    imageUrl: "/avatars/avatar22.png",
+    unlockLevel: 88,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar23",
+    description: "Avatar 23",
+    cost: 10,
+    imageUrl: "/avatars/avatar23.png",
+    unlockLevel: 92,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar24",
+    description: "Avatar 24",
+    cost: 10,
+    imageUrl: "/avatars/avatar24.png",
+    unlockLevel: 96,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar25",
+    description: "Avatar 25",
+    cost: 10,
+    imageUrl: "/avatars/avatar25.png",
+    unlockLevel: 100,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar26",
+    description: "Avatar 26",
+    cost: 10,
+    imageUrl: "/avatars/avatar26.png",
+    unlockLevel: 104,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar27",
+    description: "Avatar 27",
+    cost: 10,
+    imageUrl: "/avatars/avatar27.png",
+    unlockLevel: 108,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar28",
+    description: "Avatar 28",
+    cost: 10,
+    imageUrl: "/avatars/avatar28.png",
+    unlockLevel: 112,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar29",
+    description: "Avatar 29",
+    cost: 10,
+    imageUrl: "/avatars/avatar29.png",
+    unlockLevel: 116,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar30",
+    description: "Avatar 30",
+    cost: 10,
+    imageUrl: "/avatars/avatar30.png",
+    unlockLevel: 120,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar31",
+    description: "Avatar 31",
+    cost: 10,
+    imageUrl: "/avatars/avatar31.png",
+    unlockLevel: 124,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar32",
+    description: "Avatar 32",
+    cost: 10,
+    imageUrl: "/avatars/avatar32.png",
+    unlockLevel: 128,
+    effectValue: null
+  },
+  {
+    type: "avatar",
+    title: "avatar33",
+    description: "Avatar 33",
+    cost: 10,
+    imageUrl: "/avatars/avatar33.png",
+    unlockLevel: 132,
+    effectValue: null
+  }
+]);
