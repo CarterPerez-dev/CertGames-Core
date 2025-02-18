d4e9756c82b7946dd223e424ef5db157f144045a
Carter Perez <cartertechsolution@gmail.com>
2025-02-18 15:48:03 -0500

    Update Login.js

--- frontend/my-react-app/src/components/pages/auth/Login.js
+++ frontend/my-react-app/src/components/pages/auth/Login.js
@@ -1,10 +1,9 @@
-// src/components/pages/auth/Login.js
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loginUser } from '../store/userSlice';
 import { useNavigate, Link } from 'react-router-dom';
-import { FaEye, FaEyeSlash } from 'react-icons/fa'; // import icons
-import './Login.css'; // Ensure Auth.css is imported here or merge styles
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import './Login.css';
 import './auth.css';
 
 function hasSpacesOrInvalidChars(str) {
