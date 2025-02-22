ee90d9520e6fa37c031d66a64ab3e254b3f6123b
Carter Perez <cartertechsolution@gmail.com>
2025-02-22 15:53:56 -0500

    Update 8-DONE.js

--- tests/A+ Core 2/8-DONE.js
+++ tests/A+ Core 2/8-DONE.js
@@ -1,7 +1,7 @@
 db.tests.insertOne({
   "category": "aplus2",
   "testId": 8,
-  "testName": "A+ Core 2 Practice Test #8 (Ruthless)",
+  "testName": "A+ Core 2 Practice Test #8 (Very Challenging)",
   "xpPerCorrect": 10,
   "questions": [
     {
