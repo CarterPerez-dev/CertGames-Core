3f2b67796a6961336238d669aa992ea70f755819
Carter Perez <cartertechsolution@gmail.com>
2025-02-23 01:30:20 -0500

    Update 2-DONE.js

--- tests/A+ Core 2/2-DONE.js
+++ tests/A+ Core 2/2-DONE.js
@@ -4,6 +4,7 @@ db.tests.insertOne({
   "testName": "A+ Core 2 Practice Test #2 (Very Easy)",
   "xpPerCorrect": 10,
   "questions": [
+    {
       "id": 1,
       "question": "Which Windows tool can be used to manage startup applications and boot processes for troubleshooting purposes?",
       "options": [
