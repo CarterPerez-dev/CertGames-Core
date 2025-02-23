9c73297f7a5f61ae0d38e55be3998b4c4cbb7108
Carter Perez <cartertechsolution@gmail.com>
2025-02-23 00:42:55 -0500

    Update 3-DONE.js

--- tests/A+ Core 1/3-DONE.js
+++ tests/A+ Core 1/3-DONE.js
@@ -1,7 +1,7 @@
 db.tests.insertOne({
   "category": "aplus",
   "testId": 3,
-  "testName": "A+ Practice Test #3 (Easy)",
+  "testName": "A+ Core 1 Practice Test #3 (Easy)",
   "xpPerCorrect": 10,
   "questions": [
     {
