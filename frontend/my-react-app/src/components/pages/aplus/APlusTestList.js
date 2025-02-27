254a96eeeee4ed8038f2716950afa8845093c6b9
Carter Perez <cartertechsolution@gmail.com>
2025-02-27 17:46:15 -0500

    Update APlusTestList.js

--- frontend/my-react-app/src/components/pages/aplus/APlusTestList.js
+++ frontend/my-react-app/src/components/pages/aplus/APlusTestList.js
@@ -268,8 +268,8 @@ const APlusTestList = () => {
         <div className="popup-overlay">
           <div className="popup-content">
             <p>
-              You are currently in progress on this test. To change the test length, please finish your current attempt.
-              Restarting now will resume with your current test length.
+              You are currently in progress on this test, are you sure you want to restart!😱. Also, if you want to change the test length, please finish your current attempt.
+              Restarting now will resume with your current test length and restart your progress🧙‍♂️.
             </p>
             <div className="popup-buttons">
               <button
