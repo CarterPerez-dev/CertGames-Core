18fcc334b091495e99ff4becf25ea56aa126ff36
Carter Perez <cartertechsolution@gmail.com>
2025-02-27 11:34:23 -0500

    Create iconMapping.js

---
+++ frontend/my-react-app/src/iconMapping.js
@@ -0,0 +1,45 @@
+import {
+  FaTrophy,
+  FaMedal,
+  FaStar,
+  FaCrown,
+  FaBolt,
+  FaBook,
+  FaBrain,
+  FaCheckCircle,
+  FaRegSmile,
+  FaMagic
+} from "react-icons/fa";
+
+const iconMapping = {
+  test_rookie: FaTrophy,
+  accuracy_king: FaMedal,
+  bronze_grinder: FaBook,
+  silver_scholar: FaStar,
+  gold_god: FaCrown,
+  platinum_pro: FaMagic,
+  walking_encyclopedia: FaBrain,
+  redemption_arc: FaBolt,
+  memory_master: FaRegSmile,
+  coin_collector_5000: FaBook,
+  coin_hoarder_10000: FaBook,
+  coin_tycoon_50000: FaBook,
+  perfectionist_1: FaCheckCircle,
+  double_trouble_2: FaCheckCircle,
+  error404_failure_not_found: FaCheckCircle,
+  level_up_5: FaTrophy,
+  mid_tier_grinder_25: FaMedal,
+  elite_scholar_50: FaStar,
+  ultimate_master_100: FaCrown,
+  category_perfectionist: FaBolt,
+  absolute_perfectionist: FaBolt,
+  exam_conqueror: FaMedal,
+  subject_specialist: FaMedal,
+  answer_machine_1000: FaBook,
+  knowledge_beast_5000: FaBrain,
+  question_terminator: FaBrain,
+  test_finisher: FaCheckCircle,
+  subject_finisher: FaCheckCircle
+};
+
+export default iconMapping;
