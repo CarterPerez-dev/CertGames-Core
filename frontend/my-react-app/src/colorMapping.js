f488086a2354dfeefa281fcd52a2a55416cfe71e
Carter Perez <cartertechsolution@gmail.com>
2025-02-27 11:33:50 -0500

    Create colorMapping.js

---
+++ frontend/my-react-app/src/colorMapping.js
@@ -0,0 +1,32 @@
+const colorMapping = {
+  test_rookie: "#FFD700",           // gold
+  accuracy_king: "#C0C0C0",          // silver
+  bronze_grinder: "#cd7f32",         // bronze
+  silver_scholar: "#C0C0C0",         // silver
+  gold_god: "#FFD700",              // gold
+  platinum_pro: "#E5E4E2",          // platinum
+  walking_encyclopedia: "#6A5ACD",   // slate blue
+  redemption_arc: "#FF4500",         // orange red
+  memory_master: "#32CD32",          // lime green
+  coin_collector_5000: "#DAA520",     // goldenrod
+  coin_hoarder_10000: "#B8860B",      // dark goldenrod
+  coin_tycoon_50000: "#8B4513",       // saddle brown
+  perfectionist_1: "#00FF7F",        // spring green
+  double_trouble_2: "#00FF7F",       // spring green
+  error404_failure_not_found: "#FF6347", // tomato red
+  level_up_5: "#FFD700",             // gold
+  mid_tier_grinder_25: "#C0C0C0",      // silver
+  elite_scholar_50: "#C0C0C0",         // silver
+  ultimate_master_100: "#FFD700",      // gold
+  category_perfectionist: "#FF4500",   // orange red
+  absolute_perfectionist: "#FF4500",   // orange red
+  exam_conqueror: "#C0C0C0",           // silver
+  subject_specialist: "#C0C0C0",       // silver
+  answer_machine_1000: "#DAA520",      // goldenrod
+  knowledge_beast_5000: "#6A5ACD",     // slate blue
+  question_terminator: "#6A5ACD",      // slate blue
+  test_finisher: "#00FF7F",            // spring green
+  subject_finisher: "#00FF7F"          // spring green
+};
+
+export default colorMapping;
