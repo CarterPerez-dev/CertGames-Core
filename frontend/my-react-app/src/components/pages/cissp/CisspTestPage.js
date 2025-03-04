ed798205b43c780260524297a6dff5c62b3c2576
Carter Perez <cartertechsolution@gmail.com>
2025-03-04 16:56:17 -0500

    Update CisspTestPage.js

--- frontend/my-react-app/src/components/pages/cissp/CisspTestPage.js
+++ frontend/my-react-app/src/components/pages/cissp/CisspTestPage.js
@@ -1,5 +1,4 @@
 import React, { memo } from "react";
-import React from "react";
 import { useParams } from "react-router-dom";
 import CisspTestList from "./CisspTestList";
 import GlobalTestPage from "../../GlobalTestPage";
