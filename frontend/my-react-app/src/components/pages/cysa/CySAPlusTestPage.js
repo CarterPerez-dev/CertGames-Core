d552706c6e79a8211009488b4d7c6097b1cd023b
Carter Perez <cartertechsolution@gmail.com>
2025-03-04 16:56:38 -0500

    Update CySAPlusTestPage.js

--- frontend/my-react-app/src/components/pages/cysa/CySAPlusTestPage.js
+++ frontend/my-react-app/src/components/pages/cysa/CySAPlusTestPage.js
@@ -1,5 +1,4 @@
 import React, { memo } from "react";
-import React from "react";
 import { useParams } from "react-router-dom";
 import CySAPlusTestList from "./CySAPlusTestList";
 import GlobalTestPage from "../../GlobalTestPage";
