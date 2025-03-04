68832961e838c310d345b9ad73fcb7333b46de2e
Carter Perez <cartertechsolution@gmail.com>
2025-03-04 16:55:43 -0500

    Update APlusCore2TestPage.js

--- frontend/my-react-app/src/components/pages/aplus2/APlusCore2TestPage.js
+++ frontend/my-react-app/src/components/pages/aplus2/APlusCore2TestPage.js
@@ -1,5 +1,4 @@
 import React, { memo } from "react";
-import React from "react";
 import { useParams } from "react-router-dom";
 import AplusCore2TestList from "./AplusCore2TestList";
 import GlobalTestPage from "../../GlobalTestPage";
