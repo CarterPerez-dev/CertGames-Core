f572c01fa9ec372c3ab30fc4ad66c7cdb87fd7bc
Carter Perez <cartertechsolution@gmail.com>
2025-03-06 17:53:56 -0500

    Update 7-DONE.js

--- tests/A+ Core 1/7-DONE.js
+++ tests/A+ Core 1/7-DONE.js
@@ -1,7 +1,7 @@
 db.tests.insertOne({
   "category": "aplus",
   "testId": 7,
-  "testName": "A+ Core 1 Practice Test #7 (Challenging)",
+  "testName": "CompTIA A+ Core 1 (1101) Practice Test #7 (Challenging)",
   "xpPerCorrect": 10,
   "questions": [
     {
