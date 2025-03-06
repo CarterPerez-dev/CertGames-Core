2d2cc1b14d947adc59fee7d9e200883909571223
Carter Perez <cartertechsolution@gmail.com>
2025-03-06 17:52:34 -0500

    Update 5-DONE.js

--- tests/A+ Core 1/5-DONE.js
+++ tests/A+ Core 1/5-DONE.js
@@ -1,7 +1,7 @@
 db.tests.insertOne({
   "category": "aplus",
   "testId": 5,
-  "testName": "A+ Core 1 Practice Test #5 (Intermediate)",
+  "testName": "CompTIA A+ Core 1 (1101) Practice Test #5 (Intermediate)",
   "xpPerCorrect": 10,
   "questions": [
     {
