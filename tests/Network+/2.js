b06f49d6afe66014603b3f0cb57d10b55667269b
Carter Perez <cartertechsolution@gmail.com>
2025-03-06 18:14:32 -0500

    Update 2.js

--- tests/Network+/2.js
+++ tests/Network+/2.js
@@ -1,7 +1,7 @@
 db.tests.insertOne({
   "category": "nplus",
   "testId": 2,
-  "testName": "CompTIA Network+ (N10-009) Test #2 (Very Easy)",
+  "testName": "CompTIA Network+ (N10-009) Practice Test #2 (Very Easy)",
   "xpPerCorrect": 10,
   "questions": [
     {
