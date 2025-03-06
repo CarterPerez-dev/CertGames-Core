8370cc042687d0c73a3a45e0e80041b56bc1950f
Carter Perez <cartertechsolution@gmail.com>
2025-03-06 18:20:07 -0500

    Update 5.js

--- tests/Network+/5.js
+++ tests/Network+/5.js
@@ -1,7 +1,7 @@
 db.tests.insertOne({
   "category": "nplus",
   "testId": 5,
-  "testName": "Network+ Practice Test #5 (Intermediate)",
+  "testName": "CompTIA Network+ (1102) Practice Test #5 (Intermediate)",
   "xpPerCorrect": 10,
   "questions": [
     {
