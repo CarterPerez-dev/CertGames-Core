7ab06f51037f5e304ed3d61da6a56a1c423853ae
Carter Perez <cartertechsolution@gmail.com>
2025-03-06 15:53:46 -0500

    Update 2-VERIFY.js

--- tests/Data+/2-VERIFY.js
+++ tests/Data+/2-VERIFY.js
@@ -1,7 +1,7 @@
 db.tests.insertOne({
-  "category": "CompTIA Data+",
+  "category": "dataplus",
   "testId": 2,
-  "testName": "Practice Test #2 (Very Easy)",
+  "testName": "CompTIA Data+ Practice Test #2 (Very Easy)",
   "xpPerCorrect": 10,
   "questions": [
     {
