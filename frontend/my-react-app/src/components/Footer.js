7300aba7096cb1cdcfcc88f34bde30e185260a13
Carter Perez <cartertechsolution@gmail.com>
2025-03-07 16:48:58 -0500

    Update Footer.js

--- frontend/my-react-app/src/components/Footer.js
+++ frontend/my-react-app/src/components/Footer.js
@@ -1,17 +1,19 @@
 // src/components/Footer.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../global.css'
+import './footer.css'
 
 const Footer = () => {
   return (
     <footer className="site-footer">
-      <div className="footer-links">
-        <Link to="/">Info</Link>
-        <Link to="/privacy">Privacy Policy</Link>
-        <Link to="/terms">Terms of Service</Link>
+      <div className="footer-content">
+        <div className="footer-links">
+          <Link to="/">Home</Link>
+          <Link to="/privacy">Privacy Policy</Link>
+          <Link to="/terms">Terms of Service</Link>
+        </div>
+        <p className="footer-copyright">© {new Date().getFullYear()} Certgames.com. All rights reserved.</p>
       </div>
-      <p>© {new Date().getFullYear()} Certgames.com. All rights reserved.</p>
     </footer>
   );
 };
