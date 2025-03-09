853f1321eda378336133ced521f5483a44b7366c
Carter Perez <cartertechsolution@gmail.com>
2025-03-08 20:25:58 -0500

    Update and rename 8-DONE.js to 9-DONE.js

--- tests/A+ Core 1/9-DONE.js
+++ tests/A+ Core 1/9-DONE.js
@@ -1,7 +1,7 @@
 db.tests.insertOne({
   "category": "aplus",
-  "testId": 8,
-  "testName": "CompTIA A+ Core 1 (1101) Practice Test #8 (Very Challenging)",
+  "testId": 9,
+  "testName": "CompTIA A+ Core 1 (1101) Practice Test #8 (Ruthless)",
   "xpPerCorrect": 10,
   "questions": [
     {
