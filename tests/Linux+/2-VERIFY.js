873cb0a55d3528a83cdad0372ca51ef263a55ce8
Carter Perez <cartertechsolution@gmail.com>
2025-03-10 12:43:59 -0400

    Update 2-VERIFY.js

--- tests/Linux+/2-VERIFY.js
+++ tests/Linux+/2-VERIFY.js
@@ -1,7 +1,7 @@
 db.tests.insertOne({
   "category": "linuxplus",
   "testId": 2,
-  "testName": "Linux+ Practice Test #2 (Very Easy)",
+  "testName": "CompTIA Linux+ (XK0-005) Practice Test #2 (Very Easy)",
   "xpPerCorrect": 10,
   "questions": [
     {
