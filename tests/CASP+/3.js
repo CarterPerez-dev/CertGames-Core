ad2cf0b1f069b5d1477834fa7a5b960b1cbddf32
Carter Perez <cartertechsolution@gmail.com>
2025-03-10 14:44:19 -0400

    Update 3.js

--- tests/CASP+/3.js
+++ tests/CASP+/3.js
@@ -1,7 +1,7 @@
 db.tests.insertOne({
   "category": "caspplus",
   "testId": 3,
-  "testName": "SecurityX Practice Test #3 (Easy)",
+  "testName": "CompTIA SecurityX (CAS-005) Practice Test #3 (Easy)",
   "xpPerCorrect": 10,
   "questions": [
     {
