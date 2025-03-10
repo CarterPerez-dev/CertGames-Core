4ba2e06355c6a5397def6d30f86b100cb52852a8
Carter Perez <cartertechsolution@gmail.com>
2025-03-10 14:13:23 -0400

    Update 6.js

--- tests/CySa+/6.js
+++ tests/CySa+/6.js
@@ -1,7 +1,7 @@
 db.tests.insertOne({
   "category": "cysa",
   "testId": 6,
-  "testName": "CySa+ Practice Test #6 (Formidable)",
+  "testName": "CompTIA CySa+ (CS0-003) Practice Test #6 (Formidable)",
   "xpPerCorrect": 10,
   "questions": [
     {
